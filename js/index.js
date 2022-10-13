d625b1c41320d7097e1666de3ba14061e2b3fecb
Carla Gil <carlabgo95@gmail.com>
2022-10-13 20:03:16 -0300

    agrego funcionalidades js

--- js/index.js
+++ js/index.js
@@ -8,3 +8,36 @@ function toggleMenu(){
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+let destinos = [
+    {
+        nombre: "Jujuy",
+        img : "img/Cerrodelos7colores.jpg",
+        id: 1
+    },
+    {
+        nombre: "Usuahia",
+        img : "img/Les_Eclaireurs_Lighthouse-1024x768.jpg",
+        id: 2
+    },
+    {
+        nombre: "Mendoza",
+        img : "img/potrerillos.jpg",
+        id: 3
+    },
+];
+
+
+const $mostrar = document.getElementById("mostrar_destinos");
+
+
+function mostrarDestinos(array) {
+    $mostrar.innerHTML = array.map((item) =>
+    `<div class="destinos-array" id="${item.id}">
+    <img src="${item.img}">
+    <h3>${item.nombre}</h3>
+    </div>`
+    ).join(" ");
+}
+
+mostrarDestinos(destinos);
--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ alert ("¡Recuerde no compartir su contraseña con nadie!")
 
 document.getElementById("nombre").value = "Dafne Alonso"
 document.getElementById("correo").value="dafnecelestea@gmail.com"
-document.getElementById("usuario").value = "Dafne123"
+document.getElementById("pass").value = "Dafne123"
 
 
