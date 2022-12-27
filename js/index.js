c7def8202a194be88b90f10e8e101a7ca1736304
carla <carlabgo95@gmail.com>
2022-12-27 14:13:12 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -9,89 +9,39 @@ function toggleMenu() {
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 
-let destinos = [
-    {
-        nombre: "Jujuy",
-        img: "img/Cerrodelos7colores.jpg",
-        id: 1
-    },
-    {
-        nombre: "Usuahia",
-        img: "img/Les_Eclaireurs_Lighthouse-1024x768.jpg",
-        id: 2
-    },
-    {
-        nombre: "Mendoza",
-        img: "img/potrerillos.jpg",
-        id: 3
-    },
-];
-
-
-const $mostrar = document.getElementById("mostrar_destinos");
-
-
-function mostrarDestinos(array) {
-    $mostrar.innerHTML = array.map((item) =>
-        `<div class="destinos-array" id="${item.id}">
-    <img src="${item.img}">
-    <h3>${item.nombre}</h3>
-    </div>`
-    ).join(" ");
-}
-
-mostrarDestinos(destinos);
-// let carruselArray = [
+// let destinos = [
 //     {
-//         img: "img/bariloche.jpg",
-//         id:1
+//         nombre: "Jujuy",
+//         img: "img/Cerrodelos7colores.jpg",
+//         id: 1
 //     },
 //     {
-//         img: "img/potrerillos.jpg",
+//         nombre: "Usuahia",
+//         img: "img/Les_Eclaireurs_Lighthouse-1024x768.jpg",
 //         id: 2
 //     },
 //     {
-//         img: "img/aconcagua.jpg",
+//         nombre: "Mendoza",
+//         img: "img/potrerillos.jpg",
 //         id: 3
 //     },
-//     {
-//         img: "img/ushuaia.webp",
-//         id: 4
-//     },
-//     {
-//         img: "img/5142724_Catedral-de-Salta2.jpg",
-//         id: 5
-//     },
-//     {
-//         img: "img/laslenas.jpg",
-//         id: 6
-//     },
-//     {
-//         img: "img/Dique-El-Jumeal_Carolina-Cabrera_50.jpg",
-//         id: 7
-//     },
-//     {
-//         img: "img/cerro-siete-colores-jujuy-529939.jpg",
-//         id: 8
-//     },
-//     {
-//         img: "img/cafayate-1.jpg",
-//         id: 9
-//     },
-//     {
-//         img: "img/lasquijadas.jpg",
-//         id: 10
-//     },
-//     {
-//         img: "img/casa-historica-de-tucuman-3.jpg",
-//         id: 11
-//     },
-//     {
-//         img: "img/valledelaluna.jpg",
-//         id: 11
-//     },
 // ];
-// const $carrusel = document.getElementById("carrusel_items");
+
+
+// const $mostrar = document.getElementById("mostrar_destinos");
+
+
+// function mostrarDestinos(array) {
+//     $mostrar.innerHTML = array.map((item) =>
+//         `<div class="destinos-array" id="${item.id}">
+//     <img src="${item.img}">
+//     <h3>${item.nombre}</h3>
+//     </div>`
+//     ).join(" ");
+// }
+
+// mostrarDestinos(destinos);
+
 const carrusel = document.querySelector(".carrusel-items");
 
 let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
